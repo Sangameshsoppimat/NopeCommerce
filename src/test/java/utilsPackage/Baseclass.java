673cc9f1b9f67b07cd1d3da3dde887eaf6cdec93
sangameshsoppimat <sangameshbusines@gmail.com>
2024-05-11 08:35:26 +0530

    sgrg

--- src/test/java/utilsPackage/Baseclass.java
+++ src/test/java/utilsPackage/Baseclass.java
@@ -4,15 +4,15 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Baseclass 
 {
 	public WebDriver driver;
-	@BeforeClass
+	@BeforeMethod
 	public void setUp()
 	{
 		WebDriverManager.chromedriver().setup();
@@ -24,7 +24,7 @@ public class Baseclass
 		
 	}
 	
-	@AfterClass
+	@AfterMethod
 	public void teardown()
 	{
 		driver.close();
